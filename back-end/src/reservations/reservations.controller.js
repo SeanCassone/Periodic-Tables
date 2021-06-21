da054667cb3222d58aecbb02e4bdff2fc82a99bc
SeanCassone <sean.cassone@gmail.com>
2021-06-21 12:59:16 -0400

    completed user story 3 all tests pass

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -32,7 +32,20 @@ async function hasValidFields(req, res, next) {
 }
 
 async function hasValidDateTime(req, res, next) {
-  const { reservation_date, reservation_time, people } = req.body.data;
+  const { reservation_date, reservation_time } = req.body.data;
+  // sets currentDate to today
+  const currentDate = new Date();
+  // returns a new date instance given the current reservation date and time
+  const reservationDate = new Date(reservation_date + " " + reservation_time);
+  // gets the day of the reservation as a number. Number 2 is equal to Tuesday
+  const weekday = reservationDate.getDay();
+  // gets the reservation hour in UTC format
+  const reservationHour = reservationDate.getHours();
+  // gets the reservation minutes in UTC format
+  const reservationMinutes = reservationDate.getMinutes();
+  // formats the hours and minutes in this shape 00:00
+  const reservationTime = `${reservationHour}:${reservationMinutes}`;
+
   // returns 400 if reservation_time is not a time that matches 00:00 format
   if (!reservation_time.match(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)) {
     return next({
@@ -41,13 +54,34 @@ async function hasValidDateTime(req, res, next) {
     });
   }
   // returns 400 if reservation_date is not a valid date
-  if (isNaN(new Date(reservation_date))) {
+  if (isNaN(reservationDate)) {
     return next({
       status: 400,
       message: "reservation_date is not valid date.",
     });
   }
-
+  // returns 400 if not a future reservation
+  if (reservationDate < currentDate) {
+    return next({
+      status: 400,
+      message: "Only future reservations are allowed.",
+    });
+  }
+  // returns 400 is reservation is on a tuesday
+  if (weekday === 2) {
+    return next({
+      status: 400,
+      message:
+        "Reservation day is Tuesday. The restaurant is closed on Tuesdays .",
+    });
+  }
+  // Validates that reservation time is during buisness hours
+  if (reservationTime < "10:30" || reservationTime > "21:30") {
+    return next({
+      status: 400,
+      message: "The reservation time must be between 10:30AM and 10:30PM.",
+    });
+  }
   next();
 }
 
@@ -62,6 +96,7 @@ async function hasValidNumberOfPeople(req, res, next) {
   }
   next();
 }
+
 // List handler for reservation resources
 async function list(req, res) {
   const { date } = req.query;
