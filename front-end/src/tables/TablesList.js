616c528192a874e9f3cdf0f12b663d11b1855fed
SeanCassone <sean.cassone@gmail.com>
2021-06-22 19:16:00 -0400

    mapped tables body to display data on dashboard

--- front-end/src/tables/TablesList.js
+++ front-end/src/tables/TablesList.js
@@ -1,17 +1,7 @@
 function TablesList({ tables }) {
-  const { reservation_id, table_id, table_name, capacity } = tables;
-  return (
-    <div>
-      <table className="table table-hover table-responsive">
-        <thead className="thead-light">
-          <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Res. ID</th>
-            <th scope="col">Table Name</th>
-            <th scope="col">Capacity</th>
-            <th scope="col">Status</th>
-          </tr>
-        </thead>
+  const tablesTableBody = tables.map(
+    ({ reservation_id, table_id, table_name, capacity }) => {
+      return (
         <tbody key={table_id}>
           <tr>
             <th scope="row">{table_id}</th>
@@ -25,6 +15,22 @@ function TablesList({ tables }) {
             </td>
           </tr>
         </tbody>
+      );
+    }
+  );
+  return (
+    <div>
+      <table className="table table-hover table-responsive">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Res. ID</th>
+            <th scope="col">Table Name</th>
+            <th scope="col">Capacity</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        {tablesTableBody}
       </table>
     </div>
   );
