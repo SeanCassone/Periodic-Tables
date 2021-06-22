a3cd0a25d52cfcc46d2741b9c9c5c8e5a93712ef
SeanCassone <sean.cassone@gmail.com>
2021-06-21 20:24:21 -0400

    created seating path and component

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -6,7 +6,7 @@ import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 import CreateReservation from "../Reservations/CreateReservation";
 import CreateTableForReservation from "../tables/CreateTableForReservation";
-
+import TableSeating from "../tables/TableSeating";
 /**
  * Defines all the routes for the application.
  *
@@ -28,6 +28,9 @@ function Routes() {
       </Route>
       <Route path="/reservations/new">
         <CreateReservation />
+      </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <TableSeating />
       </Route>
       <Route path="/tables/new">
         <CreateTableForReservation />
---
+++ front-end/src/tables/TableSeating.js
@@ -0,0 +1,4 @@
+function TableSeationg(params) {
+  return <h2>Seating</h2>;
+}
+export default TableSeationg;
