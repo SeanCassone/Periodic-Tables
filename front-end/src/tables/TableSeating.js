702a63dc990781cb417556a063f98754ab70ee64
SeanCassone <sean.cassone@gmail.com>
2021-06-24 18:54:43 -0400

    removed psuedocode and console.log's

--- front-end/src/tables/TableSeating.js
+++ front-end/src/tables/TableSeating.js
@@ -3,33 +3,14 @@ import { useParams, useHistory } from "react-router-dom";
 import { readReservation, listTables, updateTable } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
 function TableSeating() {
-  /**
-   *
-   * get reservation id from params
-   *
-   * set state for form data, tables, errors
-   * need useEffect call to:
-   *    get the list of tables and set the tables state - catch errors
-   *
-   * filter tables by revervation id of null and capacity greater or equal to people
-   *
-   * create Submit button that, when clicked, assigns the table to the reservation then
-   * displays the /dashboard page,
-   * 1. PUT to /tables/:table_id/seat/ in order to save the table assignment.
-   * 2. The body of the request must be { data: { reservation_id: x } } where X is
-   *      the reservation_id of the reservation being seated
-   *
-   *  display a Cancel button that, when clicked, returns the user to the previous page
-   */
-
   const history = useHistory();
   const { reservation_id } = useParams();
-  // console.log(reservation_id);
+
   const [formData, setFormData] = useState({ table_id: "" });
   const [reservation, setReservation] = useState({});
   const [table, setTable] = useState([]);
   const [error, setError] = useState(null);
-  // console.log(reservation);
+
   useEffect(() => {
     async function loadTables() {
       const abortController = new AbortController();
