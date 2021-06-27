c6502ac89c9f123b7c00fd384c162682a9b04c8f
SeanCassone <sean.cassone@gmail.com>
2021-06-27 00:20:52 -0400

    added migrate for production

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,5 +1,6 @@
 const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
+knex.migrate.latest([config]);
 const knex = require("knex")(config);
 
 module.exports = knex;
