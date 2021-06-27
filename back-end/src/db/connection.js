1927d3aabb1ce308c7046b9fd9e83bc56fa9a9b4
SeanCassone <sean.cassone@gmail.com>
2021-06-27 13:07:55 -0400

    tests connection

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,6 +1,5 @@
 const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
-knex.migrate.latest([config]);
 const knex = require("knex")(config);
 
-module.exports = knex;
+module.exports = knex(knexConfig[environment]);
