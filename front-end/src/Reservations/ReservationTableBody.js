a73392b628659e3443f87e5a982166734f5c1a2f
SeanCassone <sean.cassone@gmail.com>
2021-06-29 16:08:32 -0400

    troubleshooting number appearing as last name

--- front-end/src/Reservations/ReservationTableBody.js
+++ front-end/src/Reservations/ReservationTableBody.js
@@ -34,9 +34,8 @@ function ReservationTableBody({ reservation }) {
         <th scope="row">{reservation.reservation_id}</th>
         <td>{reservation.reservation_date}</td>
         <td>{reservation.reservation_time}</td>
-        <td>
-          {reservation.first_name} {reservation.last_name}
-        </td>
+        <td>{reservation.first_name}</td>
+        <td>{reservation.last_name}</td>
         <td>{reservation.mobile_number}</td>
         <td>{reservation.people}</td>
         <td className="text-center">
