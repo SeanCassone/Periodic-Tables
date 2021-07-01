d896c8912a5256486a79c5acf03087c5f0245324
SeanCassone <sean.cassone@gmail.com>
2021-07-01 12:58:26 -0400

    missing - in formatDate function

--- front-end/src/utils/convertDateTimeTelephone.js
+++ front-end/src/utils/convertDateTimeTelephone.js
@@ -27,7 +27,9 @@ export function formatDate(inputDate) {
   const date = new Date(inputDate);
   if (!isNaN(date.getTime())) {
     // Months use 0 index.
-    return date.getMonth() + 1 + "-" + date.getDate() + "" + date.getFullYear();
+    return (
+      date.getMonth() + 1 + "-" + date.getDate() + "-" + date.getFullYear()
+    );
   }
 }
 
